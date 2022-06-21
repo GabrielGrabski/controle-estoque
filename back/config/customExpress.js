248dc9bd6b5306474134544061eda96d797c450a
Filipe Cupini <88508043+FilipeCupini@users.noreply.github.com>
2022-06-21 20:00:11 -0300

    #005 - Conecta banco de dados MySql

--- back/config/customExpress.js
+++ back/config/customExpress.js
@@ -3,13 +3,16 @@ const consign = require('consign')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+
 module.exports = () => {
-    const app = express();
+    const app = express()
     
     app.use(bodyParser.urlencoded({extended:true}))
     app.use(bodyParser.json())
     app.use(cors())
-    consign().include('cadastro').into(app)
-
+    consign()
+        .include('cadastro')
+        .into(app)
+    
     return app
 }
--- back/index.js
+++ back/index.js
@@ -6,9 +6,8 @@ conexao.connect(erro => {
     if(erro){
         console.log(erro)
     }else{
-        console.log('Conectado...')
-
-        const app = customExpress()
-        app.listen(3000, () => console.log('Servidor ativo na porta 3000'))
+        console.log('Concetado ao banco de dados!')        
+        const app = customExpress()        
+        app.listen(3000,() => console.log('Servidor rodando na porta 3000'))
     }
 })
